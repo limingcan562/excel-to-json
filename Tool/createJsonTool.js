eea5e1fc190936234fd0b42e3f70ed128387dbc0
limingcan562 <804666207@qq.com>
2022-01-21 00:52:44 +0800

    完成功能开发，准备编写功能说明文档

--- Tool/createJsonTool.js
+++ Tool/createJsonTool.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const colors = require('colors');
 const ora = require('ora');
-
+const chalk = require('chalk');
 
 /**
  * @startRow 从第几行开始拿值(默认第2行)
@@ -12,95 +12,108 @@ const ora = require('ora');
  * @fileName 要转换的excel表格
  */
 
-
-module.exports = ({
-    startRow = 2,
-    fileName,
-    importPath = './doc',
-    outPath,
-    fixedKeyName,
-    commonKey = 'category'
-}) => {
-    // 读取excel中所有工作表的数据
-    const 
-    xlsxList = xlsxrd.parse(path.join(__dirname, `${importPath}${fileName}.xlsx`)),
-    finalResult = [];
-
-    // console.log(xlsxList[0].data);
-
-    xlsxList.forEach(item => {
+module.exports = {
+    create({startRow = 2, fileName, importPath, outPath, outJsonName, fixedKeyName}) {
+        // console.log(this);
+        // 读取excel中所有工作表的数据
         const 
-        xlsxData = item.data,
-        xlsxName = item.name,
-        keyData = xlsxData[startRow - 1], // 每项数据里的key
-        renderData = xlsxData.slice(2), // 每项数据里的key 里面对应的value
-        fixedKeyIndex = keyData.findIndex(item => item === fixedKeyName),
-        xlsxEveryData = {},
-        categoryData = [];
+        xlsxList = xlsxrd.parse(path.join(__dirname, `${importPath}${fileName}.xlsx`)),
+        finalResult = []; // 最后要输出的json数据，有多少个sheet 就有多少个json
 
-        xlsxEveryData['xlsxName'] = xlsxName;
-        // console.log(`${'[info]'.bold} 开始生成${xlsxName}.json文件`.yellow);
-        const createAni = ora(`正在生成${xlsxName}.json文件`);
-        // createAni.start();
-        // console.log(keyData);
-        // console.log(renderData);
-        // console.log(fixedKeyIndex);
-        // console.log(xlsxData);
+        // console.log(xlsxList[0].data);
+        // console.log(xlsxList);
+        for (let index = 0; index < xlsxList.length; index++) {
+            const 
+            item = xlsxList[index]
+            xlsxData = item.data.filter(item => item.length !== 0), // Excel 转成的一行行数据
+            xlsxName = item.name ? item.name : `表格${index + 1}`, // Excel 表格里面的名字
+            keyData = xlsxData[startRow - 1], // 每项数据里的key
+            renderData = xlsxData.slice(startRow), // 每项数据里的key 里面对应的value
+            fixedKeyIndex = keyData.findIndex(item => item === fixedKeyName), // Excel里面是不是有固定字段
+            finalXlsxEveryData = {}, // Excel 最后得得到的每个 sheet 组装好的数据
+            categoryData = [];  // 将每个值以key: value && 含有类别的数组
 
 
-        if (fixedKeyIndex === -1) {
-            createAni.fail('请确定Excel表格有'+ fixedKeyName + '字段');
-            return;
+            finalXlsxEveryData['xlsxName'] = xlsxName;
+
+            // console.log(fixedKeyIndex);
+            const createAni = ora(`正在重组${chalk.yellow(xlsxName)} 表格文件`);
+            if (fixedKeyIndex === -1) {
+                createAni.fail(`请检查Excel表格有${chalk.red(fixedKeyName)}字段`);
+                break;
+            }
+
+            // console.log(xlsxData);
+            createAni.start();
+            // 先将类别放进去自己组建的数组对象
+            renderData.forEach(singleArr => {
+                const prefixKey = singleArr[fixedKeyIndex];
+                // console.log(prefixKey);
+                // console.log(singleArr);
+                let obj = {};
+                singleArr.forEach((item2, index) => {
+                    const key = keyData[index];
+                    obj[key] = item2;
+                });
+                categoryData.push(obj);
+            });
+            // createAni.text = '获取到重组含类别数据';
+            // console.log(categoryData);
+            // console.log(finalXlsxEveryData);
+
+
+            // 重组最后要的数据
+            categoryData.forEach(item => {
+                const commonKey = item[fixedKeyName];
+                // console.log(commonKey);
+                // ! 如果没有这个key的数组，则创建这个数组
+                if (!finalXlsxEveryData[commonKey]) finalXlsxEveryData[commonKey] = [];
+
+                // ! 如果有这个key的数组，则添加
+                let 
+                obj = {},
+                defaultValue = '';
+                keyData.forEach(keyName => {
+                    if (keyName !== fixedKeyName) {
+                        obj[keyName] = item[keyName] ? item[keyName] : defaultValue;
+                    }
+                });
+                finalXlsxEveryData[commonKey].push(obj);
+            });
+            // console.log(finalXlsxEveryData);
+
+            // 获取最后的数据
+            // createAni.text = '获取最后的数据';
+            finalResult.push(finalXlsxEveryData);
+
+            // 开始输出json
+            createAni.succeed(`${chalk.yellow(xlsxName)} 表格数据重组成功`);
+            let exportAni = ora(`正在输出${chalk.yellow(xlsxName)} 表格json文件`).start();
+            this.exportJson(finalXlsxEveryData, outPath, outJsonName[index] ? outJsonName[index] : xlsxName, exportAni);
         }
-        
-        // 先把相同的 类别 归类
-        renderData.forEach((renderDataItem) => {
-            // console.log(renderDataItem);
-            let obj = {};
-            renderDataItem.forEach((item2, index) => {
-                const key = keyData[index];
-                obj[key] = item2;
-            });
-            categoryData.push(obj);
-        });
-        // console.log(categoryData);
+    },
 
-        // 获得最后渲染的数据
-        categoryData.forEach((item, index) => {
-            const key = item[commonKey];
-            if (!xlsxEveryData[key]) xlsxEveryData[key] = [];
-
-            let obj = {};
-            keyData.forEach(keyName => {
-                obj[keyName] = item[keyName] ? item[keyName] : '';
-            });
-            xlsxEveryData[key].push(obj);
-        });
-        // console.log(xlsxEveryData);
-        finalResult.push(xlsxEveryData);
-    });
-
-    // console.log(finalResult);
-
-    finalResult.forEach(item => {
-        const {xlsxName} = item;
+    // 输出json
+    exportJson(everyList, outPath, outJsonName, exportAni) {
+        // console.log(everyList);
+        // 开始输出json
         new Promise((resolve, reject) => {
-            fs.writeFile(path.join(__dirname, `${outPath}${xlsxName}.json`), JSON.stringify(item), err => {
+            fs.writeFile(path.join(__dirname, `${outPath}${outJsonName}.json`), JSON.stringify(everyList), err => {
                 if (err) {
                     // throw err;
                     reject(err)
                 } else {
-                    // console.log(`${'[success]'.bold} ${outName}.json生成成功`.green);
+                    // console.log(`${'[success]'.bold} ${outJsonName}.json生成成功`.green);
                     resolve();
                 }
             });
         })
         .then(() => {
-            console.log(`${'[success]'.bold} ${xlsxName}.json文件生成成功`.green);
+            exportAni.succeed(`${chalk.yellow(`${outJsonName}.json`)} 文件生成成功`);
         })
         .catch(err => {
             // console.err(JSON.stringify(`${xlsxName}.json文件生成错误：${JSON.stringify(err)}`));
             console.log(err);
-        })
-    });
+        });
+    }
 }
--- Tool/index.js
+++ Tool/index.js
@@ -1,7 +1,9 @@
-const createJson = require('./createJsonTool');
-createJson({
+const Tool = require('./createJsonTool');
+
+Tool.create({
     importPath: '../excel/',
     outPath: '../json/',
+    outJsonName: ['config1', 'config2', 'config3'],
     fileName: '配置文件',
     fixedKeyName: 'fixedKey' 
 });
